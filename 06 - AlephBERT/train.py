9a7d1a6d30a17392d4f784d4407848a7dfcfce5b
idan <idankash@gmail.com>
2023-06-24 15:58:09 +0300

    trainer has a bug

--- 06 - AlephBERT/train.py
+++ 06 - AlephBERT/train.py
@@ -1,64 +1,73 @@
 import numpy as np
-import torch
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from transformers import Trainer, TrainingArguments
-from transformers import BertModel, BertTokenizerFast
-from transformers import AdamW, get_linear_schedule_with_warmup
+import torch
 from datasets import Dataset
+from sklearn.model_selection import train_test_split
+from torch.optim import AdamW
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments, \
+    get_linear_schedule_with_warmup, default_data_collator
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-alephbert_tokenizer = BertTokenizerFast.from_pretrained('onlplab/alephbert-base')
-alephbert = BertModel.from_pretrained('onlplab/alephbert-base')
-
-
-# TODO: set freezed / non-freezed layers
-
 df = pd.read_csv("train.csv")
 
-lyricists = df['lyricist']
-lyrics = df['lyrics']
+lyricists = df['lyricist'].to_list()
+lyrics = df['lyrics'].to_list()
 
 X_train, X_val, y_train, y_val = train_test_split(lyrics, lyricists, test_size=0.15, stratify=lyricists)
 train_df = pd.DataFrame(np.c_[X_train, y_train], columns=['lyrics', 'lyricist'])
 val_df = pd.DataFrame(np.c_[X_val, y_val], columns=['lyrics', 'lyricist'])
 
+#train_df['lyrics'] = train_df['lyrics'].apply(lambda x: ' '.join(x.split()[:512]))
+#val_df['lyrics'] = val_df['lyrics'].apply(lambda x: ' '.join(x.split()[:512]))
+
 train_dataset = Dataset.from_pandas(train_df)
 val_dataset = Dataset.from_pandas(val_df)
 
-optimizer = AdamW(alephbert.parameters(), lr=1e-3, weight_decay=0.01)
-lr_scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=1e3, num_training_steps=1e4)
+alephbert_tokenizer = AutoTokenizer.from_pretrained('onlplab/alephbert-base')
+alephbert = AutoModelForSequenceClassification.from_pretrained("onlplab/alephbert-base",
+                                                               num_labels=len(df["lyricist"].unique()))
+alephbert.config.id2label = {i: label for i, label in enumerate(df["lyricist"].unique())}
+alephbert.config.label2id = {label: i for i, label in enumerate(df["lyricist"].unique())}
 
+# TODO: set freezed / non-freezed layers
+# alephbert.classifier.out_features = len(df['lyricist'].unique())
+for p in alephbert.bert.embeddings.parameters():
+    p.requires_grad = False
 
-args = {
-    "output_dir": "output",
-    "overwrite_output_dir": True,
-    "do_train": True,
-    "per_device_train_batch_size": 16,
-    "per_device_eval_batch_size": 8,
-    "fp16": True,
-    "logging_dir": "logs",
-    "logging_steps": 100,
-    "evaluation_strategy": "steps",
-    "eval_steps": 10,
-    "save_steps": 100,
-    "save_total_limit": 1,
-    "load_best_model_at_end": True,
-    "metric_for_best_model": "accuracy",
-    "seed": 42,
-    "full_determinism": False
-}
+print(alephbert)
+args = TrainingArguments(
+    "output",
+    evaluation_strategy="steps",
+    eval_steps=10,
+    save_steps=100,
+    save_total_limit=5,
+    load_best_model_at_end=True,
+    num_train_epochs=10,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=8,
+    learning_rate=1e-3,
+    weight_decay=0.01,
+    warmup_steps=int(1e3),
+    logging_dir="logs",
+    logging_steps=100,
+    fp16=True,
+    seed=42,
+)
 
-alephbert.to(device)
+optimizer = AdamW(alephbert.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+lr_scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=args.warmup_steps,
+                                               num_training_steps=args.max_steps)
+
 
 trainer = Trainer(
     model=alephbert,
+    args=args,
     train_dataset=train_dataset,
     eval_dataset=val_dataset,
     tokenizer=alephbert_tokenizer,
-    optimizers=[optimizer, lr_scheduler],
-    args=TrainingArguments(**args)
+    data_collator=default_data_collator,
+    optimizers=(optimizer, lr_scheduler),
 )
 
 trainer.train()
